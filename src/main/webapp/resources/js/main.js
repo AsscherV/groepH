fea05b827d17b8eb4e155b451c9a9be476e05f27
Vincent Asscher <vincent.asscher@gmail.com>
2013-02-06 13:44:55 +0100

    css & js resources added

---
+++ src/main/webapp/resources/js/main.js
@@ -0,0 +1,25 @@
+
+	$(document).ready(function(){
+		$(".tabContentsTrips").hide(); // Hide all tab conten divs by default
+		$(".tabContentsTrip").hide();
+		$(".tabContentsTrips:first").show(); // Show the first div of tab content by default
+		$(".tabContentsTrip:first").show();
+		
+		$("#tabContainerTrips ul li a").click(function(){ //Fire the click event
+			
+			var activeTab = $(this).attr("href"); // Catch the click link
+			$("#tabContainerTrips ul li a").removeClass("active"); // Remove pre-highlighted link
+			$(this).addClass("active"); // set clicked link to highlight state
+			$(".tabContentsTrips").hide(); // hide currently visible tab content div
+			$(activeTab).fadeIn(); // show the target tab content div by matching clicked link.
+		});
+		
+		$("#tabContainerTrip ul li a").click(function(){ //Fire the click event
+			
+			var activeTab = $(this).attr("href"); // Catch the click link
+			$("#tabContainerTrip ul li a").removeClass("active"); // Remove pre-highlighted link
+			$(this).addClass("active"); // set clicked link to highlight state
+			$(".tabContentsTrip").hide(); // hide currently visible tab content div
+			$(activeTab).fadeIn(); // show the target tab content div by matching clicked link.
+		});
+	});
