0f633bb06287ef3e1bdffe1ebbb2108e2524fa44
Vincent Asscher <vincent.asscher@gmail.com>
2013-02-07 11:01:25 +0100

    Update register in index.xhtml + js + css

--- src/main/webapp/resources/js/main.js
+++ src/main/webapp/resources/js/main.js
@@ -1,25 +1,36 @@
 
-	$(document).ready(function(){
-		$(".tabContentsTrips").hide(); // Hide all tab conten divs by default
-		$(".tabContentsTrip").hide();
-		$(".tabContentsTrips:first").show(); // Show the first div of tab content by default
-		$(".tabContentsTrip:first").show();
-		
-		$("#tabContainerTrips ul li a").click(function(){ //Fire the click event
-			
-			var activeTab = $(this).attr("href"); // Catch the click link
-			$("#tabContainerTrips ul li a").removeClass("active"); // Remove pre-highlighted link
-			$(this).addClass("active"); // set clicked link to highlight state
-			$(".tabContentsTrips").hide(); // hide currently visible tab content div
-			$(activeTab).fadeIn(); // show the target tab content div by matching clicked link.
-		});
-		
-		$("#tabContainerTrip ul li a").click(function(){ //Fire the click event
-			
-			var activeTab = $(this).attr("href"); // Catch the click link
-			$("#tabContainerTrip ul li a").removeClass("active"); // Remove pre-highlighted link
-			$(this).addClass("active"); // set clicked link to highlight state
-			$(".tabContentsTrip").hide(); // hide currently visible tab content div
-			$(activeTab).fadeIn(); // show the target tab content div by matching clicked link.
-		});
+$(document).ready(function(){
+	$(".tabContentsTrips").hide(); // Hide all tab conten divs by default
+	$(".tabContentsTrip").hide();
+	$(".tabContentsTrips:first").show(); // Show the first div of tab content by default
+	$(".tabContentsTrip:first").show();
+
+	$("#tabContainerTrips ul li a").click(function(){ //Fire the click event
+
+		var activeTab = $(this).attr("href"); // Catch the click link
+		$("#tabContainerTrips ul li a").removeClass("active"); // Remove pre-highlighted link
+		$(this).addClass("active"); // set clicked link to highlight state
+		$(".tabContentsTrips").hide(); // hide currently visible tab content div
+		$(activeTab).fadeIn(); // show the target tab content div by matching clicked link.
+
+                       $(".button").fadeIn(); // show unvisible trip tabs
 	});
+
+	$("#tabContainerTrip ul li a").click(function(){ //Fire the click event
+
+		var activeTab = $(this).attr("href"); // Catch the click link
+		$("#tabContainerTrip ul li a").removeClass("active"); // Remove pre-highlighted link
+		$(this).addClass("active"); // set clicked link to highlight state
+		$(".tabContentsTrip").hide(); // hide currently visible tab content div
+		$(activeTab).fadeIn(); // show the target tab content div by matching clicked link.
+	});
+
+         $("#register").click(function(){ //Fire the click event
+
+		var activeTab = $(this).attr("href"); // Catch the click link
+		$("#tabContainerTrip ul li a").removeClass("active"); // Remove pre-highlighted link
+        $(".button").hide(); // hide visible tabs
+		$(".tabContentsTrip").hide(); // hide currently visible tab content div
+		$(activeTab).fadeIn(); // show the target tab content div by matching clicked link.
+	});
+});
