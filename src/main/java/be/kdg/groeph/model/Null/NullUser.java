4a11514213cd197950f0a6f35df2c4f750f526cb
Gunther <gunther.laurijssens@student.kdg.be>
2013-02-12 15:12:25 +0100

    /

--- src/main/java/be/kdg/groeph/model/Null/NullUser.java
+++ src/main/java/be/kdg/groeph/model/Null/NullUser.java
@@ -1,11 +1,11 @@
 package be.kdg.groeph.model.Null;
 
-import be.kdg.groeph.model.User;
+import be.kdg.groeph.model.TripUser;
 
 /**
  * To change this template use File | Settings | File Templates.
  */
-public class NullUser extends User implements Nullable{
+public class NullUser extends TripUser implements Nullable{
     @Override
     public boolean isNull() {
         return true;
